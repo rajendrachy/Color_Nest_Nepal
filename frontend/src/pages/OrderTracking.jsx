14d0f9bbe0678024f0aaafb188b2998cb19f1a60
rajendrachy <chyrajendra32@gmail.com>
2026-04-23 18:43:12 +0530

    add practice sheeds ans

--- frontend/src/pages/OrderTracking.jsx
+++ frontend/src/pages/OrderTracking.jsx
@@ -213,14 +213,11 @@ const OrderTracking = () => {
                   zoom={13} 
                   scrollWheelZoom={false} 
                   style={{ height: '100%', width: '100%' }}
-                  attributionControl={false}
                 >
                   <TileLayer
                     url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    attribution='&copy; OpenStreetMap'
                   />
-                  <AttributionControl position="bottomright" prefix={false} />
-                  
                   
                   <Marker position={destination} icon={icons.destination}>
                     <Popup>
